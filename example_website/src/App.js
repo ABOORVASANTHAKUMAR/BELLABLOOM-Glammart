59c15ca900508e34f75abbc3d9db93957487899a
ANUSREE S S <sreeanu110405@gmail.com>
2023-11-21 20:36:26 +0530

    Second Commit

--- example_website/src/App.js
+++ example_website/src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React from 'react';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Shop from './Pages/Shop';
+import ShopCategory from './Pages/ShopForm';
+import Product from './Pages/Product';
+import Cart from './Pages/Cart';
+import Login from './Pages/Login';
+import SignUp from './Pages/SignUp';    
+import Footer from './Components/Footer/Footer';
+import men_banner from './Components/Assests/banner_mens.png';
+import women_banner from './Components/Assests/banner_women.png';
+import kid_banner from './Components/Assests/banner_kids.png';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Shop/>}/>
+          <Route path='/makeup' element={<ShopCategory banner={men_banner} category="makeups"/>}/>
+          <Route path='/skin' element={<ShopCategory banner={men_banner} category="skins"/>}/>
+          <Route path='/hair' element={<ShopCategory banner={women_banner} category="hairs"/>}/>
+          <Route path='/health' element={<ShopCategory banner={kid_banner} category="healths"/>}/>
+          <Route path="/product" element={<Product/>}>
+            <Route path=':productId' element={<Product/>}/>
+          </Route>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<SignUp/>} />
+        </Routes>
+        <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
