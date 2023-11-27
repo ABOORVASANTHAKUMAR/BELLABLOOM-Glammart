f7f22c1247126bb8361dd6d4245191d45228dfa3
ANUSREE S S <136184913+anusree-ss-110405@users.noreply.github.com>
2023-11-27 16:11:23 +0530

    Assests Image Commit

---
+++ example_website/src/data.js
@@ -0,0 +1,52 @@
+
+import SKIN2 from './SKIN2.jpg';
+import m1 from './m1.jpg';
+import m2 from './m2.jpg';
+import h1 from './h1.jpg';
+import h2 from './h2.jpg';
+
+let data_product = [
+  
+  {
+    id: 2,
+    name: "Cetaphil Brightening Night Cream with Niacinamide reduces Dark spots, Dermatologist Tested (50gm)",
+    category: "skin",
+    image:SKIN2,
+    new_price: 649,
+    old_price: 409,
+  },
+  {
+    id: 1,
+    name: "COLOR WOW Dream Coat Supernatural Spray - Keep Your Hair Frizz-Free and Shiny No Matter the Weather with Award-Winning Anti-Frizz Spray",
+    category: "Hair",
+    image:h1,
+    new_price: 999,
+    old_price: 599,
+  },
+  {
+    id: 2,
+    name: "Sugarbear Hair Vitamins Extra Strength Biotin 6000mcg, Vitamin C, E, Coconut Oil, Zinc, Folic Acid, Inositol - Vegan Gummies for Luscious Hair and Nails - Supplement for Women & Men (1 Month Supply))",
+    category: "Hair",
+    image:h2,
+    new_price: 649,
+    old_price: 409,
+  },
+  {
+    id: 21,
+    name: "LAVONE Eyebrow Stamp Pencil Kit for Eyebrows, Makeup Brow Stamp Trio Kit with Waterproof Eyebrow Pencil, Eyeliner, Eyebrow Pomade, and Dual-ended Eyebrow Brush - Dark Brown",
+    category: "Makeup",
+    image:m1 ,
+    new_price: 85.0,
+    old_price: 120.5,
+  },
+  {
+    id: 22,
+    name: "TATCHA The Deep Cleanse | Deep, Gentle Exfoliating Cleanser, Lifts Dirt, Minimizes Excess Oil & Unclogs & Tightens Pores",
+    category: "Makeup",
+    image: m2,
+    new_price: 85.0,
+    old_price: 120.5,
+  }
+]
+
+export default data_product;
